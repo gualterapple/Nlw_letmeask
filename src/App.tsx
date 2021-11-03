8fa3345c871cc559c4377be2b84a1b7e1345f707
gualterapple <39972000+gualterapple@users.noreply.github.com>
2021-11-03 05:55:50 +0000

    Navegação entre as páginas, feito.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Button } from './components/Button';
+import { BrowserRouter, Route } from 'react-router-dom'; 
+
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <div>
-      <NewRoom />
-    </div>
+    <BrowserRouter>
+      <Route path="/" component={Home} exact/>
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
